a9090d0664cd1283597eb265e4ac42659a06f31f
RJ Castellano <rj.castellano@ciit.edu.ph>
2025-11-09 17:41:02 +0800

    feat(products):Add backend query for filtering

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -3,7 +3,15 @@ import Product from "../models/Product.js";
 // GET /products - fetch all products
 export const getProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    //For fitlering by category
+    const { category } = req.query;
+    const filter = {};
+
+    if (category) {
+      filter.category = { $regex: new RegExp(`^${category}$`, "i") };
+    }
+
+    const products = await Product.find(filter);
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
