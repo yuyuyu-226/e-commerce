1382cc70f11cf5eadf6a6491f365ea9153f8937c
RJ Castellano <rj.castellano@ciit.edu.ph>
2025-11-10 12:10:09 +0800

    feat(products):Add backend query for sorting

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -4,17 +4,44 @@ import Product from "../models/Product.js";
 export const getProducts = async (req, res) => {
   try {
     //For fitlering by category
-    const { category } = req.query;
+    const { category, sort } = req.query;
     const filter = {};
 
     if (category) {
       filter.category = { $regex: new RegExp(`^${category}$`, "i") };
     }
 
-    const products = await Product.find(filter);
+    //For sorting by price
+    let sortOption = {};
+    if (sort === "price_asc") {
+      sortOption.price = 1; // Ascending
+    } else if (sort === "price_desc") {
+      sortOption.price = -1; // Descending
+    }
+
+    //fetch products from DB based on filter and sort options
+    const products = await Product.find(filter).sort(sortOption);
+
     res.json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getCategories = async (req, res) => {
+  try {
+    // Fetch distinct categories from active products
+    const categories = await Product.distinct("category", { status: "active" });
+
+    //Filter out any null or empty strings, then sort alphabetically
+    const cleanCategories = categories
+      .filter((c) => c && c.trim() !== "")
+      .sort();
+
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching unique categories:", error);
     res.status(500).json({ error: "Server error" });
   }
 };
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getProducts } from "../controllers/productController.js";
+import {
+  getProducts,
+  getCategories,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
 //fetch all products
 router.get("/getProducts", getProducts);
 
+//fetch list of unique categories
+router.get("/getCategories", getCategories);
 export default router;
